a9d397e94850ef13c9e654e8df04232c51d4e825
Dana Palmer <roiai4@hotmail.com>
2018-05-11 01:31:48 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,14 @@
+var recipes = {}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+return Object.assign({}, object, { [key]: value})
+}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return object
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  
+}
